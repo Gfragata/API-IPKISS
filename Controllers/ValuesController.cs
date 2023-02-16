007971b861a71ed876f96a5fbb5db5ebba391d73
Gfragata <54363635+Gfragata@users.noreply.github.com>
2023-02-16 16:46:37 -0300

    Subida dos arquivos de desenvolvimento

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_IPKISS.Controllers
+{
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet("balance")]
+        public ActionResult balance(int account_id)
+        {
+            if(account_id == 1234){
+                return NotFound();
+            } else
+            {
+                return Ok();
+            }
+        }
+
+        [HttpPost("reset")]
+        public ActionResult reset()
+        {
+            return Ok();
+        }
+    }
+}
